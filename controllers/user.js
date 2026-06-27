73c91224c7a83cfc928cd404c675aa4e19b5f859
Aditya Soni <adityasoni95847124@gmail.com>
2026-06-27 23:55:23 +0530

    1st deploy

--- controllers/user.js
+++ controllers/user.js
@@ -62,7 +62,7 @@ const sendAuthResponse = (res, user, message) => {
 
   res.cookie("token", token, {
     httpOnly: true,
-    secure: process.env.NODE_ENV === "production",
+    secure: process.env.NODE_ENV == "production",
     sameSite: "none",
     maxAge: 7 * 24 * 60 * 60 * 1000,
   });
